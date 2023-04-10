96e884a8d05a40091610ba0e6c6b3f2c94831849
Ando144 <andoni.otzdezarate@gmail.com>
2023-04-10 21:38:05 +0200

    el documento de drive sin cambios

---
+++ PmoboProiektua/src/xakea/Dorrea.java
@@ -0,0 +1,15 @@
+package xakea;
+
+public class Dorrea extends Pieza {
+	
+    public Dorrea(boolean kolorea, int errenkada, int zutabea) {
+        super(kolorea, errenkada, zutabea);
+    }
+
+    @Override
+    public boolean mugituDaiteke(int zeinErrenkadara, int zeinZutabera) {
+    	boolean ahalDu = true;
+    	//...
+    	return ahalDu;
+    }
+}
